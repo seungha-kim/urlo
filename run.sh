6289d4b5b5d6794e46b348334470b9b38fb98772
Kim Seungha <seungha.me@gmail.com>
2017-03-18 10:26:48 +0000

    envlist

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 mkdir -p `realpath ~/data`
 docker build . -t urlo
-docker --rm -d --name=urlo -p 8000:8000 --env-file ./env.list -v `realpath ~/data`:/app/data urlo
+docker kill urlo
+docker rm urlo
+docker run -d --name=urlo -p 8000:8000 --env-file ./env.list -v `realpath ~/data`:/app/data urlo
